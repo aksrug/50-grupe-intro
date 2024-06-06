d43c0b3ebe7d476ea0b8ff9fc8f9320b87c5f78b
FIRSTNAME LASTNAME <YOUR@EMAIL.COM>
2024-06-06 11:46:06 +0300

    uzduotys update

--- ND/manto.uzduotys.js
+++ ND/manto.uzduotys.js
@@ -294,7 +294,190 @@ calculate(A);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+const B = [
+    'the quick brown fox',
+    'jumps over the lazy dog',
+    'a bird in the hand',
+    'is worth two in the bush',
+    'an apple a day',
+    'keeps the doctor away',
+    'actions speak louder than words',
+    'all good things must come to an end',
+    'beauty is in the eye of the beholder',
+    'better late than never',
+    'birds of a feather flock together',
+    'cleanliness is next to godliness',
+    'dont bite the hand that feeds you',
+    'dont count your chickens before they hatch',
+    'dont put all your eggs in 1 basket',
+    'every cloud has a silver lining',
+    'every dog has its day',
+    'every rose has its thorn',
+    'fortune favors the bold',
+    'good things come to those who wait',
+    'a watched pot never boils',
+    'haste makes waste',
+    'history repeats itself',
+    'cat got your tongue',
+    'home is where the heart is',
+    'if the shoe fits, wear it',
+    'ignorance is bliss',
+    'it takes two to tango',
+    'arabs have 100 words for sand',
+    'kill two birds with one stone',
+    'laughter is the best medicine',
+    'love is blind',
+    'money doesnt grow on trees',
+    'no pain no gain',
+    'nothing ventured nothing gained',
+    'once bitten twice shy',
+    'practice makes perfect',
+    'the early bird catches the worm',
+    'the grass is always greener on the other side',
+    'the pen is mightier than 10 swords',
+    'theres no smoke without fire',
+    'time heals all wounds',
+    '5 second rule',
+    'every dog has its day',
+    'to err is human to forgive divine',
+    '2 wrongs dont make a right',
+    'when in Rome do as the Romans do',
+    'where theres smoke, theres fire',
+    'you cant judge a book by its cover',
+    'you cant make an omelette without breaking eggs',
+    'you reap what you sow',
+    'youre never too old to learn'
+];
 
+//Surasti ilgiausią stringą;
+
+function longestString(arr) {
+    let string = "";
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length > string.length) {
+            string = arr[i];
+
+        }
+    }
+    return console.log(string);
+}
+
+longestString(B);
+
+
+// Surasti trumpiausią stringą;
+
+function shortestString(array) {
+    let shortest = array[0];
+    console.log(shortest);
+
+    for (let i = 0; i < array.length; i++) {
+        if (shortest.length > array[i].length) {
+            shortest = array[i];
+        }
+    }
+    return console.log(shortest);
+}
+
+shortestString(B);
+
+
+// Surasti pirma stringa, kuris prasideda “a” raide;
+
+function startWithA(array) {
+    let string = "";
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].startsWith("a")) {
+            string = array[i];
+            break;
+        }
+    }
+    return console.log(string);
+}
+startWithA(B);
+
+//Paskaiciuoti, kiek yra stringu, kurie prasideda "a" raide
+
+function startWithA(array) {
+    let string = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].startsWith("a")) {
+            string++;
+        }
+    }
+    return console.log(string);
+}
+startWithA(B);
+
+//Isrinkti visus stringus, kurie prasideda raide "a"
+
+function startWithA(array) {
+    let stringArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].startsWith("a")) {
+            stringArray.push(array[i])
+        }
+    }
+    return console.log(stringArray);
+}
+startWithA(B);
+
+
+// Surasti stringą su daugiausia žodžių;
+
+function longest(array) {
+    let stringLongest = "";
+    let maxWordsCount = 0;
+    for (let i = 0; i < array.length; i++) {
+        let wordCount = array[i].split(" ").length;
+        console.log(wordCount);
+        if (wordCount > maxWordsCount) {
+            maxWordsCount = wordCount;
+            stringLongest = array[i];
+
+        }
+
+    }
+    return console.log(`Ilgiausias stringas: ${stringLongest} jis turi ${maxWordsCount} zodziu`);
+}
+longest(B);
+
+
+// Surasti pirma stringą su mažiausiai žodžių;
+
+function shortest(array) {
+    let shortest = array[0];
+    
+    for (let i = 0; i < array.length; i++) {
+        
+        if (array[i].split(" ").length < shortest.split(" ").length) {
+            shortest = array[i];
+
+        }
+
+    }
+    return console.log(shortest);
+}
+shortest(B);
+
+
+// Suskaičiuoti, kiek stringų turi daugiau nei 4 žodžius;
+
+function fourAndMore (array) {
+    let fourAndMore = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length > )
+    }
+return console.log(fourAndMore);
+}
+fourAndMore(B);
+
+
+
+// Suskaičiuoti kiek masyve yra žodžių;
+// Suskaičiuoti visas ‘s’ raides;
+// Suskaičiuoti kiek masyve yra raidžių (tarpų tarp žodžių neskaičiuoti!);
+// Surasti ilgiausią žodį (ne stringą, o atskirą stringo žodį!);
 
 
 
