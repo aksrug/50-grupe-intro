a61eaed4c8c0a3338a326a85ef80be872831f6d0
FIRSTNAME LASTNAME <YOUR@EMAIL.COM>
2024-06-07 14:21:10 +0300

    uzduociu update

--- ND/manto.uzduotys.js
+++ ND/manto.uzduotys.js
@@ -346,7 +346,7 @@ const B = [
     'you cant judge a book by its cover',
     'you cant make an omelette without breaking eggs',
     'you reap what you sow',
-    'youre never too old to learn'
+    'youre never too old to learn'   
 ];
 
 //Surasti ilgiausią stringą;
@@ -463,21 +463,99 @@ shortest(B);
 
 // Suskaičiuoti, kiek stringų turi daugiau nei 4 žodžius;
 
-function fourAndMore (array) {
-    let fourAndMore = array[0];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].length > )
+function moreThan4(arr) {
+    let ans = 0;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i].split(' ').length > 4) {
+            ans++;
+        }
     }
-return console.log(fourAndMore);
+    return ans;
 }
-fourAndMore(B);
-
+console.log(moreThan4(B));
 
 
 // Suskaičiuoti kiek masyve yra žodžių;
+
+function countWords(arr) {
+    let ans = 0;
+    for (i = 0; i < arr.length; i++) {
+        ans += arr[i].split(' ').length;
+    }
+    return ans;
+}
+console.log(countWords(B));
+
+
 // Suskaičiuoti visas ‘s’ raides;
+
+function countS(arr) {
+    let ans = 0;
+    for (i = 0; i < arr.length; i++) {
+        for (let index = 0; index < arr[i].length; index++) {
+            if (arr[i][index] === 's') {
+                ans++;
+            }
+
+        }
+    }
+    return ans
+}
+console.log(countS(B))
+
+
+
 // Suskaičiuoti kiek masyve yra raidžių (tarpų tarp žodžių neskaičiuoti!);
+
+function countLetters(arr) {
+    let ans = 0
+    for (i = 0; i < arr.length; i++) {
+        for (let index = 0; index < arr[i].length; index++) {
+            if (arr[i][index] !== ' ') {
+                ans++;
+            }
+
+        }
+    }
+    return ans
+}
+console.log(countLetters(B))
+
+
+/*function countLetters2(arr) {
+    let ans = 0
+    for (i = 0; i < arr.length; i++) {
+        for (let index = 0; index < arr[i].length; index++) {
+            let regex = /^[a-zA-Z]+$/;
+            if (regex.test(arr[i][index])) {
+                ans++;
+            }/
+
+        }
+    }
+    return ans
+}
+console.log(countLetters2(B))
+*/
+
+
 // Surasti ilgiausią žodį (ne stringą, o atskirą stringo žodį!);
+
+function longestWord(arr) {
+    let ans = '';
+    for (i = 0; i < arr.length; i++) {
+        const list = arr[i].split(' ');
+        for (let index = 0; index < list.length; index++) {
+            if (list[index].length > ans.length) {
+                ans = list[index];
+            }
+
+        }
+    }
+    return ans;
+}
+console.log(longestWord(B));
+
 
 
 
